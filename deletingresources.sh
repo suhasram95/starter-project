e4e5252a7a11aaaef0588b8565ec90d23d0926aa
Suhas Ramesh <suhasram95@gmail.com>
2021-05-29 21:03:59 -0500

    Added Files

--- deletingresources.sh
+++ deletingresources.sh
@@ -2,4 +2,4 @@
 # this utility will delete all the resources
 KIND=$1
 RESOURCENAME=$2
-kubectl get $KIND $RESOURCENAME | awk '{print $1}' | grep -v NAME | xarags kubectl delete $KIND $RESOURCENAME
+kubectl get $KIND $RESOURCENAME | awk '{print $1}' | grep -v NAME | xargs kubectl delete $KIND $RESOURCENAME
